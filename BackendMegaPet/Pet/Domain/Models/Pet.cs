6bff11be936d532562a9e16944e0aa536f41603e
piers5001 <64555167+piers5001@users.noreply.github.com>
2022-06-27 00:25:27 -0500

    Add: Pet Controller

---
+++ BackendMegaPet/Pet/Domain/Models/Pet.cs
@@ -0,0 +1,12 @@
+﻿namespace BackendMegaPet.Pet.Domain.Models;
+
+public class Pet
+{
+    public int Id { get; set; }
+    public string name { get; set; }
+    public string lastname { get; set; }
+    public string gender { get; set; }
+    public string age { get; set; }
+    public string status { get; set; }
+    public string description { get; set; }
+}
